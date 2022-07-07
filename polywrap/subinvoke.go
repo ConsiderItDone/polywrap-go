f6786111d0af987edb178811d3dcb5100b2b053a
ramil <ramilexe@gmail.com>
2022-07-07 03:38:19 +0300

    PL-30 env PL-36 invoke, subinvoke

--- polywrap/subinvoke.go
+++ polywrap/subinvoke.go
@@ -2,16 +2,53 @@ package polywrap
 
 import "unsafe"
 
-//go:wasm-module w3
-//export __w3_subinvoke
-func __w3_subinvoke(uriPtr, uriLen, modulePtr, moduleLen, methodPtr, methodLen, inputPtr, inputLen uint32)
+//go:wasm-module wrap
+//export __wrap_subinvoke
+func __wrap_subinvoke(uriPtr, uriLen, methodPtr, methodLen, inputPtr, inputLen uint32) bool
 
-func W3Subinvoke(uri, module, method string, input []byte) {
+// Subinvoke Result
+
+//go:wasm-module wrap
+//export __wrap_subinvoke_result_len
+func __wrap_subinvoke_result_len() uint32
+
+//go:wasm-module wrap
+//export __wrap_subinvoke_result
+func __wrap_subinvoke_result(ptr uint32)
+
+// Subinvoke Error
+
+//go:wasm-module wrap
+//export __wrap_subinvoke_error_len
+func __wrap_subinvoke_error_len() uint32
+
+//go:wasm-module wrap
+//export __wrap_subinvoke_error
+func __wrap_subinvoke_error(ptr uint32)
+
+func WrapSubinvoke(uri, method string, input []byte) {
 	uriPtr := unsafe.Pointer(&uri)
-	modulePtr := unsafe.Pointer(&module)
 	methodPtr := unsafe.Pointer(&method)
 	inputPtr := unsafe.Pointer(&input)
 
-	__w3_subinvoke(*(*uint32)(uriPtr), uint32(len(uri)), *(*uint32)(modulePtr), uint32(len(module)),
-		*(*uint32)(methodPtr), uint32(len(method)), *(*uint32)(inputPtr), uint32(len(input)))
+	result := __wrap_subinvoke(*(*uint32)(uriPtr), uint32(len(uri)), *(*uint32)(methodPtr), uint32(len(method)),
+		*(*uint32)(inputPtr), uint32(len(input)))
+
+	if !result {
+		errorLen := __wrap_subinvoke_error_len()
+		errorBuf := make([]byte, errorLen)
+		errorPtr := unsafe.Pointer(&errorBuf)
+
+		__wrap_subinvoke_error(*(*uint32)(errorPtr))
+
+		// TODO return Result.Err
+	}
+
+	resultLen := __wrap_subinvoke_result_len()
+	resultBuf := make([]byte, resultLen)
+	resultPtr := unsafe.Pointer(&resultBuf)
+
+	__wrap_subinvoke_result(*(*uint32)(resultPtr))
+
+	// TODO return Result.Ok
 }
